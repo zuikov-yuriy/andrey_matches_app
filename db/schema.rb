fe4725fc1fc27883f6064fc8faf7e7436ddd3cf3
Юрий Зуйков <zu@MacBook-Air-Urij.local>
2019-07-16 17:59:11 +0300

    changed sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_11_210346) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "matches", force: :cascade do |t|
     t.string "title"
     t.datetime "game_time"
